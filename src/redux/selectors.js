724b7e718bc76061dcfe4038d6e20bd99b9757d2
Sebastian Lalek <chemiczq@gmail.com>
2023-12-06 09:45:59 +0100

    add auth selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,6 @@
 export const selectContacts = state => state.phonebook.contacts;
 export const selectFilter = state => state.filter.filter;
+
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectUser = state => state.auth.user;
+export const selectIsRefreshing = state => state.auth.isRefreshing;

a8dc66acbed78d58fc335bf1d566027f5e15669b
Sebastian Lalek <chemiczq@gmail.com>
2023-12-06 09:47:41 +0100

    implement Redux Persist lib

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
+
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+
+const authPersitConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
     phonebook: contactsReducer,
     filter: filterReducer,
+    auth: persistReducer(authPersitConfig, authReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+const persistor = persistStore(store);

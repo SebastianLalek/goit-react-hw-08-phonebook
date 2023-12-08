edfb7c8f12dcd23c3461336da2f9364f96c9437b
SebastianLalek <123250026+SebastianLalek@users.noreply.github.com>
2023-12-08 22:39:57 +0000

    Deploying to gh-pages from @ SebastianLalek/goit-react-hw-08-phonebook@c85d6a3e4254dbcbef926832a8bc56b3e1ad8f9c 🚀

---
+++ static/js/970.db982f95.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[970],{7970:function(e,t,s){s.r(t),s.d(t,{default:function(){return o}});var c="Home_section__1GQ45",n="Home_hero__jXgT2",r="Home_title__5eO91",a="Home_subtitle__vP3Vr",l=s(184);function o(){return(0,l.jsx)("section",{className:c,children:(0,l.jsxs)("div",{className:n,children:[(0,l.jsx)("h1",{className:r,children:"PHONEBOOK"}),(0,l.jsx)("p",{className:a,children:"Build your own contact list"})]})})}}}]);
+//# sourceMappingURL=970.db982f95.chunk.js.map

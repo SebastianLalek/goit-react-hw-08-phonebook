e463844fc8b2bf749123f501124f6c57c975c337
Sebastian Lalek <chemiczq@gmail.com>
2023-12-08 17:50:22 +0100

    add UserMenu component

---
+++ src/components/userMenu/UserMenu.jsx
@@ -0,0 +1,23 @@
+import { useAuth } from 'hooks/useAuth';
+import { useDispatch } from 'react-redux';
+import { logout } from 'redux/auth/authOperations';
+
+import css from './UserMenu.module.css';
+
+export default function UserMenu() {
+  const dispatch = useDispatch();
+
+  const { user } = useAuth();
+
+  const logoutUser = () => {
+    dispatch(logout());
+  };
+  return (
+    <div className={css.wrapper}>
+      <p className={css.user}>{user}</p>
+      <button className={css.button} onClick={logoutUser}>
+        Logout
+      </button>
+    </div>
+  );
+}
